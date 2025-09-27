dbf538e35efc494a219c63b783cfb253fa572010
alphameo <pavel.2014.play@gmail.com>
2025-09-27 13:51:55 +0300

    build: add lombok dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,11 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+	
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
 }
 
 testing {
