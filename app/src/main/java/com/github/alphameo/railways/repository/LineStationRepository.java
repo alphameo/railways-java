01a0bcbafb545fea40c8157da6445d617680c332
alphameo <pavel.2014.play@gmail.com>
2025-09-30 15:27:42 +0300

    refactor(repository): update composite ids

--- app/src/main/java/com/github/alphameo/railways/repository/LineStationRepository.java
+++ app/src/main/java/com/github/alphameo/railways/repository/LineStationRepository.java
@@ -1,6 +1,7 @@
 package com.github.alphameo.railways.repository;
 
 import com.github.alphameo.railways.domain.LineStation;
+import com.github.alphameo.railways.domain.LineStation.LineStationId;
 
 public interface LineStationRepository extends Repository<LineStation, LineStationId> {
 
--- app/src/main/java/com/github/alphameo/railways/repository/TrainCompositionCarriageRepository.java
+++ app/src/main/java/com/github/alphameo/railways/repository/TrainCompositionCarriageRepository.java
@@ -1,8 +1,9 @@
 package com.github.alphameo.railways.repository;
 
 import com.github.alphameo.railways.domain.TrainCompositionCarriage;
+import com.github.alphameo.railways.domain.TrainCompositionCarriage.TrainCompositionCarriageId;
 
 public interface TrainCompositionCarriageRepository
-        extends Repository<TrainCompositionCarriage, TrainCompositionCarriageId> {
+                                extends Repository<TrainCompositionCarriage, TrainCompositionCarriageId> {
 
 }
