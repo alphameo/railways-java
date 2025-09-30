1334e8e41670825b9fa453c965d246f3f08d3229
alphameo <pavel.2014.play@gmail.com>
2025-09-30 22:46:29 +0300

    fix(inmemory/repository): fix Locomotive repo naming

--- app/src/main/java/com/github/alphameo/railways/repository/inmemory/InMemoryLocomotiveRepository.java
+++ app/src/main/java/com/github/alphameo/railways/repository/inmemory/InMemoryLocomotiveRepository.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.github.alphameo.railways.domain.Locomotive;
 import com.github.alphameo.railways.repository.Repository;
 
-public class InMemoryLocomotive implements Repository<Locomotive, Long> {
+public class InMemoryLocomotiveRepository implements Repository<Locomotive, Long> {
 
     private final InMemoryStorage<Locomotive, Long> storage = new InMemoryStorage<Locomotive, Long>();
     private final AtomicLong idGenerator = new AtomicLong(0);
