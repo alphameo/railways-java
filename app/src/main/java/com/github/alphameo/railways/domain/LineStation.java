dadbbdb99e46d39c650345a8febb1084c99ec4ba
alphameo <pavel.2014.play@gmail.com>
2025-09-30 14:40:50 +0300

    refactor(domain): group ids into one composite id class

--- app/src/main/java/com/github/alphameo/railways/domain/LineStation.java
+++ app/src/main/java/com/github/alphameo/railways/domain/LineStation.java
@@ -1,15 +1,47 @@
 package com.github.alphameo.railways.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class LineStation {
 
-    private Long lineId;
-    private Long stationId;
+    @Setter(AccessLevel.NONE)
+    private final LineStationId id = new LineStationId();
     private Integer position;
+
+    public LineStation(Long lineId, Long stationId, Integer position) {
+        this.id.lineId = lineId;
+        this.id.stationId = stationId;
+        this.position = position;
+    }
+
+    public Long getLineId() {
+        return this.id.lineId;
+    }
+
+    public Long getStationId() {
+        return this.id.stationId;
+    }
+
+    public void setLineId(Long id) {
+        this.id.lineId = id;
+    }
+
+    public void setStationId(Long id) {
+        this.id.stationId = id;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class LineStationId {
+
+        private Long lineId;
+        private Long stationId;
+    }
 }
