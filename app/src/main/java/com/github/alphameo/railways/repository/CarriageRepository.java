687e341ecc0e3b7a4045d46c008d0d551798f525
alphameo <pavel.2014.play@gmail.com>
2025-10-02 22:22:53 +0300

    feat(repository): add CarriageRepository::findByNumber()

--- app/src/main/java/com/github/alphameo/railways/repository/CarriageRepository.java
+++ app/src/main/java/com/github/alphameo/railways/repository/CarriageRepository.java
@@ -1,7 +1,10 @@
 package com.github.alphameo.railways.repository;
 
+import java.util.Optional;
+
 import com.github.alphameo.railways.domain.Carriage;
 
 public interface CarriageRepository extends Repository<Carriage, Long> {
 
+    public Optional<Carriage> findByNumber(String number);
 }
