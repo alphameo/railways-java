14ca627695e67511cc40c115e528a8e8d21a3e4a
alphameo <pavel.2014.play@gmail.com>
2025-10-05 00:15:58 +0300

    feat(repository): add TrainRepository::findByNumber() to interface

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/TrainRepository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/TrainRepository.java
@@ -1,7 +1,10 @@
 package com.github.alphameo.railways.domain.repositories;
 
+import java.util.Optional;
+
 import com.github.alphameo.railways.domain.entities.Train;
 
 public interface TrainRepository extends Repository<Train, Long> {
 
+    public Optional<Train> findByNumber(String number);
 }
