2b7e72a2d8803561233608863639220e109f37dc
alphameo <pavel.2014.play@gmail.com>
2025-10-04 23:28:58 +0300

    feat(repository): add LocomotiveRepository::findByName()

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/LocomotiveRepository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/LocomotiveRepository.java
@@ -1,7 +1,11 @@
 package com.github.alphameo.railways.domain.repositories;
 
+import java.util.Optional;
+
 import com.github.alphameo.railways.domain.entities.Locomotive;
 
 public interface LocomotiveRepository extends Repository<Locomotive, Long> {
+
+    public Optional<Locomotive> findByNumber(String number);
 
 }
