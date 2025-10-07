5ed2cec150fb73a54ffb39a202e9b83eb08ba746
alphameo <pavel.2014.play@gmail.com>
2025-10-07 19:24:57 +0300

    feat(repositories): add findBySpecificId methods to interfaces
    for repositories of entities with composite prinary key

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/TrainCompositionCarriageRepository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/TrainCompositionCarriageRepository.java
@@ -1,8 +1,14 @@
 package com.github.alphameo.railways.domain.repositories;
 
+import java.util.List;
+
 import com.github.alphameo.railways.domain.entities.TrainCompositionCarriage;
 import com.github.alphameo.railways.domain.entities.TrainCompositionCarriage.TrainCompositionCarriageId;
 
-public interface TrainCompositionCarriageRepository extends Repository<TrainCompositionCarriage, TrainCompositionCarriageId> {
+public interface TrainCompositionCarriageRepository
+        extends Repository<TrainCompositionCarriage, TrainCompositionCarriageId> {
 
+    List<Long> findTrainCompositionIdsByCarriageId(Long trainCompositionId);
+
+    List<Long> findCarriageIdsByTrainCompositionId(Long carriageId);
 }
