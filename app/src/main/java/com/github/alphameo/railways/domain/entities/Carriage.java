7463b048d6e79c1b9bb32a199b9819595ccb697e
alphameo <pavel.2014.play@gmail.com>
2025-10-10 10:05:46 +0300

    refactor(domain): rewrite Carriage entity

--- app/src/main/java/com/github/alphameo/railways/domain/entities/Carriage.java
+++ app/src/main/java/com/github/alphameo/railways/domain/entities/Carriage.java
@@ -1,5 +1,7 @@
 package com.github.alphameo.railways.domain.entities;
 
+import com.github.alphameo.railways.domain.valueobjects.CarriageContentType;
+import com.github.alphameo.railways.domain.valueobjects.MachineNumber;
 import com.github.alphameo.railways.exceptions.domain.ValidationException;
 
 import lombok.Data;
@@ -7,24 +9,28 @@ import lombok.Data;
 @Data
 public class Carriage {
 
-    public enum ContentType {
-        PASSENGER,
-        CARGO;
-    }
-
     private Long id;
-    private String number;
-    private ContentType contentType;
+    private MachineNumber number;
+    private CarriageContentType contentType;
     private Long capacity;
 
-    public Carriage(final Long id, final String number, final ContentType contentType, final Long capacity) {
+    public Carriage(final Long id, final MachineNumber number,
+            final CarriageContentType contentType, final Long capacity) {
         this.id = id;
         this.setNumber(number);
         this.contentType = contentType;
         this.setCapacity(capacity);
     }
 
-    public void setNumber(final String number) {
+    public Carriage(final Carriage carriage) {
+        new Carriage(
+                carriage.id,
+                carriage.number,
+                carriage.contentType,
+                carriage.capacity);
+    }
+
+    public void setNumber(final MachineNumber number) {
         if (number == null) {
             throw new ValidationException("Carriage.number cannot be null");
         }
@@ -33,8 +39,11 @@ public class Carriage {
     }
 
     public void setCapacity(final Long capacity) {
-        if (capacity < 0) {
-            throw new ValidationException("Carriage.capacity must be >= 0");
+        if (capacity == null) {
+            throw new ValidationException("Carriage.capacity cannot be null");
+        }
+        if (capacity <= 0) {
+            throw new ValidationException("Carriage.capacity must be > 0");
         }
 
         this.capacity = capacity;
