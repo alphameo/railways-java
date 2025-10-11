e989b45a51350b651a07e716f0689df87882e8d7
alphameo <pavel.2014.play@gmail.com>
2025-10-11 12:16:46 +0300

    refactor(domain): rewrite Station as aggregate root

--- app/src/main/java/com/github/alphameo/railways/domain/entities/Station.java
+++ app/src/main/java/com/github/alphameo/railways/domain/entities/Station.java
@@ -1,5 +1,10 @@
 package com.github.alphameo.railways.domain.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.alphameo.railways.domain.valueobjects.ObjectName;
+import com.github.alphameo.railways.domain.valueobjects.StationLocation;
 import com.github.alphameo.railways.exceptions.domain.ValidationException;
 
 import lombok.Data;
@@ -8,16 +13,19 @@ import lombok.Data;
 public class Station {
 
     private Long id;
-    private String name;
-    private String location;
+    private ObjectName name;
+    private StationLocation location;
+    private Set<Long> lineIds;
 
-    public Station(final Long id, final String name, final String location) {
+    public Station(final Long id, final ObjectName name, final StationLocation location,
+            final Set<Long> lineIds) {
         this.id = id;
         this.setName(name);
         this.setLocation(location);
+        this.setLineIds(lineIds);
     }
 
-    public void setName(final String name) {
+    public void setName(final ObjectName name) {
         if (name == null) {
             throw new ValidationException("Station.name cannot be null");
         }
@@ -25,11 +33,44 @@ public class Station {
         this.name = name;
     }
 
-    public void setLocation(final String location) {
+    public void setLocation(final StationLocation location) {
         if (location == null) {
             throw new ValidationException("Station.location cannot be null");
         }
 
         this.location = location;
+    }
+
+    public void setLineIds(Set<Long> lineIds) {
+        if (lineIds == null) {
+            this.lineIds = new HashSet<>();
+        }
+
+        final var newIds = new HashSet<Long>();
+
+        for (Long id : lineIds) {
+            if (id == null) {
+                throw new ValidationException("lineId cannot be null");
+            }
+            newIds.add(id);
+        }
+
+        this.lineIds = lineIds;
+    }
+
+    public Set<Long> getLineIds() {
+        return new HashSet<>(this.lineIds);
+    }
+
+    public void connectToLine(Long lineId) {
+        this.lineIds.add(lineId);
+    }
+
+    public void disconnectFromLine(Long lineId) {
+        this.lineIds.remove(lineId);
+    }
+
+    public int getStationCount() {
+        return this.lineIds.size();
     }
 }
