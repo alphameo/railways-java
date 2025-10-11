1a713c6cb60fbc33febcefa2ce693efc551d5921
alphameo <pavel.2014.play@gmail.com>
2025-10-11 23:30:14 +0300

    refactor(domain): rm extra repositories

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.alphameo.railways.domain.repositories;
-
-import com.github.alphameo.railways.domain.entities.Schedule;
-
-public interface ScheduleRepository extends Repository<Schedule, Long> {
-
-}
