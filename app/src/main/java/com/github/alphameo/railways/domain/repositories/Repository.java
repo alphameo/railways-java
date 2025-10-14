4ddcc51eaed2f335342c796bc59090606d8303ae
alphameo <pavel.2014.play@gmail.com>
2025-10-14 16:02:23 +0300

    refactor(domain/repositories): return void in some common methods

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/Repository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/Repository.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 
 public interface Repository<T, ID> {
 
-    T create(T entity);
+    void create(T entity);
 
     Optional<T> findById(ID id);
 
     List<T> findAll();
 
-    T update(T entity);
+    void update(T entity);
 
     void deleteById(ID id);
 }
