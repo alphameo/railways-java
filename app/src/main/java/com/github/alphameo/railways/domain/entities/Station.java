f6bca1e41018e89419e236fab6150236766132e5
alphameo <pavel.2014.play@gmail.com>
2025-10-14 15:00:46 +0300

    refactor(domain): rm info about line from station

--- app/src/main/java/com/github/alphameo/railways/domain/entities/Station.java
+++ app/src/main/java/com/github/alphameo/railways/domain/entities/Station.java
@@ -1,8 +1,5 @@
 package com.github.alphameo.railways.domain.entities;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.github.alphameo.railways.domain.valueobjects.ObjectName;
 import com.github.alphameo.railways.domain.valueobjects.StationLocation;
 import com.github.alphameo.railways.exceptions.domain.ValidationException;
@@ -15,22 +12,18 @@ public class Station {
     private Long id;
     private ObjectName name;
     private StationLocation location;
-    private Set<Long> lineIds;
 
-    public Station(final Long id, final ObjectName name, final StationLocation location,
-            final Set<Long> lineIds) {
+    public Station(final Long id, final ObjectName name, final StationLocation location) {
         this.id = id;
         this.setName(name);
         this.setLocation(location);
-        this.setLineIds(lineIds);
     }
 
     public Station(Station station) {
         new Station(
                 station.id,
                 station.name,
-                station.location,
-                station.lineIds);
+                station.location);
     }
 
     public void setName(final ObjectName name) {
@@ -47,38 +40,5 @@ public class Station {
         }
 
         this.location = location;
-    }
-
-    public void setLineIds(Set<Long> lineIds) {
-        if (lineIds == null) {
-            this.lineIds = new HashSet<>();
-        }
-
-        final var newIds = new HashSet<Long>();
-
-        for (Long id : lineIds) {
-            if (id == null) {
-                throw new ValidationException("lineId cannot be null");
-            }
-            newIds.add(id);
-        }
-
-        this.lineIds = lineIds;
-    }
-
-    public Set<Long> getLineIds() {
-        return new HashSet<>(this.lineIds);
-    }
-
-    public void connectToLine(Long lineId) {
-        this.lineIds.add(lineId);
-    }
-
-    public void disconnectFromLine(Long lineId) {
-        this.lineIds.remove(lineId);
-    }
-
-    public int getStationCount() {
-        return this.lineIds.size();
     }
 }
