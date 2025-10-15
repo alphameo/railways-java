13002f2a1c5dfcb9d52e01fd159acef7fcb33b0c
alphameo <pavel.2014.play@gmail.com>
2025-10-15 20:11:18 +0300

    refactor(domain/repositories): update LocomotiveRepository type of number: String -> MachineNumber

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/LocomotiveRepository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/LocomotiveRepository.java
@@ -3,9 +3,10 @@ package com.github.alphameo.railways.domain.repositories;
 import java.util.Optional;
 
 import com.github.alphameo.railways.domain.entities.Locomotive;
+import com.github.alphameo.railways.domain.valueobjects.MachineNumber;
 
 public interface LocomotiveRepository extends Repository<Locomotive, Long> {
 
-    public Optional<Locomotive> findByNumber(String number);
+    public Optional<Locomotive> findByNumber(MachineNumber number);
 
 }
