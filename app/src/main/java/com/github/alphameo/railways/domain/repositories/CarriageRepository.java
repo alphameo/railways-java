e251c2fdc730394370310ab558f2c6a57c740793
alphameo <pavel.2014.play@gmail.com>
2025-10-15 19:11:16 +0300

    refactor(domain/repositories): CarriageRepository in method String -> MachineNumber

--- app/src/main/java/com/github/alphameo/railways/domain/repositories/CarriageRepository.java
+++ app/src/main/java/com/github/alphameo/railways/domain/repositories/CarriageRepository.java
@@ -3,8 +3,9 @@ package com.github.alphameo.railways.domain.repositories;
 import java.util.Optional;
 
 import com.github.alphameo.railways.domain.entities.Carriage;
+import com.github.alphameo.railways.domain.valueobjects.MachineNumber;
 
 public interface CarriageRepository extends Repository<Carriage, Long> {
 
-    public Optional<Carriage> findByNumber(String number);
+    public Optional<Carriage> findByNumber(MachineNumber number);
 }
