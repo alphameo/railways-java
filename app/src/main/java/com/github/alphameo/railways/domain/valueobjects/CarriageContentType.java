b5c47ef9f932cc5841ddb237072161f4e83e25cf
alphameo <pavel.2014.play@gmail.com>
2025-10-21 13:45:30 +0300

    featdomain/valueobjects): fabric for CarriageContentType enum

--- app/src/main/java/com/github/alphameo/railways/domain/valueobjects/CarriageContentType.java
+++ app/src/main/java/com/github/alphameo/railways/domain/valueobjects/CarriageContentType.java
@@ -3,4 +3,17 @@ package com.github.alphameo.railways.domain.valueobjects;
 public enum CarriageContentType {
     PASSENGER,
     CARGO;
+
+    public static CarriageContentType create(final String type) {
+        if (type == null) {
+            return null;
+        }
+        final String processed = type.trim().toLowerCase();
+        return switch (processed) {
+            case "passenger" -> PASSENGER;
+            case "cargo" -> CARGO;
+            case "none", "null" -> null;
+            default -> throw new IllegalArgumentException(String.format("Unknown carriage content type: $s", type));
+        };
+    }
 }
