65d96734fa41c06c2814a90808270439334a3d36
alphameo <pavel.2014.play@gmail.com>
2025-10-23 18:15:10 +0300

    feat(cli): add all into app

--- app/src/main/java/com/github/alphameo/railways/App.java
+++ app/src/main/java/com/github/alphameo/railways/App.java
@@ -16,19 +16,31 @@ import com.github.alphameo.railways.application.cli.commands.locomotive.FindLoco
 import com.github.alphameo.railways.application.cli.commands.locomotive.ListLocomotivesCommand;
 import com.github.alphameo.railways.application.cli.commands.locomotive.RegisterLocomotiveCommand;
 import com.github.alphameo.railways.application.cli.commands.locomotive.UnregisterLocomotiveCommand;
+import com.github.alphameo.railways.application.cli.commands.station.FindStationByIdCommand;
+import com.github.alphameo.railways.application.cli.commands.station.ListStationCommand;
+import com.github.alphameo.railways.application.cli.commands.station.RegisterStationCommand;
+import com.github.alphameo.railways.application.cli.commands.station.UnregisterStationCommand;
+import com.github.alphameo.railways.application.cli.commands.train.FindTrainByIdCommand;
+import com.github.alphameo.railways.application.cli.commands.train.ListTrainsCommand;
+import com.github.alphameo.railways.application.cli.commands.train.RegisterTrainCommand;
+import com.github.alphameo.railways.application.cli.commands.train.UnregisterTrainCommand;
+import com.github.alphameo.railways.application.cli.commands.traincomposition.AssembleLocomotiveCommand;
+import com.github.alphameo.railways.application.cli.commands.traincomposition.DisassembleTrainCompositionCommand;
+import com.github.alphameo.railways.application.cli.commands.traincomposition.FindTrainCompositionByIdCommand;
+import com.github.alphameo.railways.application.cli.commands.traincomposition.ListTrainCompositionsCommand;
 import com.github.alphameo.railways.application.services.CarriageService;
 import com.github.alphameo.railways.application.services.LineService;
 import com.github.alphameo.railways.application.services.LocomotiveService;
+import com.github.alphameo.railways.application.services.StationService;
+import com.github.alphameo.railways.application.services.TrainCompositionService;
+import com.github.alphameo.railways.application.services.TrainService;
 import com.github.alphameo.railways.infrastructure.inmemory.InMemoryStorage;
 
 public class App {
     public static void main(String[] args) {
         final var inMemStorage = InMemoryStorage.getInstance();
-        final var carServ = new CarriageService(inMemStorage.getCarrRepo());
-        final var lineServ = new LineService(inMemStorage.getLineRepo(), inMemStorage.getStationRepo());
-        final var locoServ = new LocomotiveService(inMemStorage.getLocoRepo());
-        final var cliApp = new CliApp();
 
+        final var carServ = new CarriageService(inMemStorage.getCarrRepo());
         final var carrMod = new CliModule("carriage");
         carrMod.addCmd(new RegisterCarriageCommand(carServ));
         carrMod.addCmd(new ListCarriagesCommand(carServ));
@@ -36,6 +48,7 @@ public class App {
         carrMod.addCmd(new FindCarriageByNumberCommand(carServ));
         carrMod.addCmd(new UnregisterCarriageCommand(carServ));
 
+        final var lineServ = new LineService(inMemStorage.getLineRepo(), inMemStorage.getStationRepo());
         final var lineMod = new CliModule("line");
         lineMod.addCmd(new DeclareLineCommand(lineServ));
         lineMod.addCmd(new FindLineByIdCommand(lineServ));
@@ -43,15 +56,45 @@ public class App {
         lineMod.addCmd(new DisbandLineCommand(lineServ));
         lineMod.addCmd(new ListLineStationsCommand(lineServ));
 
+        final var locoServ = new LocomotiveService(inMemStorage.getLocoRepo());
         final var locoMod = new CliModule("locomotive");
         locoMod.addCmd(new RegisterLocomotiveCommand(locoServ));
         locoMod.addCmd(new ListLocomotivesCommand(locoServ));
         locoMod.addCmd(new FindLocomotiveByIdCommand(locoServ));
         locoMod.addCmd(new UnregisterLocomotiveCommand(locoServ));
 
+        final var stationServ = new StationService(inMemStorage.getStationRepo());
+        final var stationMod = new CliModule("station");
+        stationMod.addCmd(new RegisterStationCommand(stationServ));
+        stationMod.addCmd(new ListStationCommand(stationServ));
+        stationMod.addCmd(new FindStationByIdCommand(stationServ));
+        stationMod.addCmd(new UnregisterStationCommand(stationServ));
+
+        final var trainServ = new TrainService(inMemStorage.getTrainRepo(), inMemStorage.getTrainCompoRepo());
+        final var trainMod = new CliModule("train");
+        trainMod.addCmd(new RegisterTrainCommand(trainServ));
+        trainMod.addCmd(new ListTrainsCommand(trainServ));
+        trainMod.addCmd(new FindTrainByIdCommand(trainServ));
+        trainMod.addCmd(new UnregisterTrainCommand(trainServ));
+
+        final var trainCompoServ = new TrainCompositionService(
+            inMemStorage.getTrainCompoRepo(),
+            inMemStorage.getCarrRepo(),
+            inMemStorage.getLocoRepo()
+        );
+        final var trainCompoMod = new CliModule("traincomposition");
+        trainCompoMod.addCmd(new AssembleLocomotiveCommand(trainCompoServ));
+        trainCompoMod.addCmd(new ListTrainCompositionsCommand(trainCompoServ));
+        trainCompoMod.addCmd(new FindTrainCompositionByIdCommand(trainCompoServ));
+        trainCompoMod.addCmd(new DisassembleTrainCompositionCommand(trainCompoServ));
+
+        final var cliApp = new CliApp();
         cliApp.addModule(carrMod);
         cliApp.addModule(lineMod);
         cliApp.addModule(locoMod);
+        cliApp.addModule(stationMod);
+        cliApp.addModule(trainMod);
+        cliApp.addModule(trainCompoMod);
         cliApp.run();
     }
 }
