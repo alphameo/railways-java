e07c5eb4b5e451b55c5ba8badd9620354a85bd4c
alphameo <pavel.2014.play@gmail.com>
2025-10-23 10:30:32 +0300

    feat(cli): renderer

---
+++ app/src/main/java/com/github/alphameo/railways/application/cli/Renderer.java
@@ -0,0 +1,55 @@
+package com.github.alphameo.railways.application.cli;
+
+import java.util.List;
+
+public class Renderer {
+
+    public static String PROMPT = "> ";
+
+    public String showHelp(String[] options) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("---Choose option---\n");
+        for (var opt : options) {
+            sb.append(opt);
+        }
+
+        return sb.toString();
+    }
+
+    public static String renderOptions(final String title, final String[] options) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(renderTitle(title));
+        sb.append("\n");
+        for (int i = 0; i < options.length; i++) {
+            sb.append(String.format("  %s. %s\n", i + 1, options[i]));
+        }
+        sb.append(PROMPT);
+
+        return sb.toString();
+    }
+
+    public static <T> String renderList(String title, List<T> list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("[ %s ]\n", title));
+        if (list.isEmpty()) {
+            sb.append("  (empty)\n");
+        }
+        for (T item : list) {
+            sb.append(String.format("  - %s\n", item.toString()));
+        }
+
+        return sb.toString();
+    }
+
+    public static String renderTitle(String title) {
+        return String.format("===[ %s ]===", title);
+    }
+
+    public static String renderSection(String title) {
+        return String.format("---[ %s ]---", title);
+    }
+
+    public static String renderOption(String title) {
+        return String.format("<> %s", title);
+    }
+}
