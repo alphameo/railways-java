970814b0a2c3b5b016dead45fa311c8ebb1aea9f
alphameo <pavel.2014.play@gmail.com>
2025-10-23 17:36:24 +0300

    feat(cli): add commands for traincomposition

---
+++ app/src/main/java/com/github/alphameo/railways/application/cli/commands/traincomposition/ListTrainCompositionsCommand.java
@@ -0,0 +1,44 @@
+package com.github.alphameo.railways.application.cli.commands.traincomposition;
+
+import com.github.alphameo.railways.application.cli.Renderer;
+import com.github.alphameo.railways.application.cli.commands.CliCommand;
+import com.github.alphameo.railways.application.services.TrainCompositionService;
+import com.github.alphameo.railways.exceptions.application.cli.CliArgsCountException;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class ListTrainCompositionsCommand implements CliCommand {
+
+    private static String NAME = "list";
+    private final String SIGNATURE = String.format("%s", NAME);
+    @Setter
+    private String[] args;
+    private TrainCompositionService service;
+
+    public ListTrainCompositionsCommand(@NonNull final TrainCompositionService service) {
+        this.service = service;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getSignature() {
+        return SIGNATURE;
+    }
+
+    @Override
+    public void execute() {
+        final int argsCount = 0;
+        if (args.length != argsCount) {
+            throw new CliArgsCountException("==", argsCount);
+        }
+        final var list = this.service.listAll();
+        System.out.println(Renderer.renderList("All Locomotives", list));
+    }
+}
