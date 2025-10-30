72763a7b19efca0aebe425641ae9e97f6c165114
alphameo <pavel.2014.play@gmail.com>
2025-10-30 23:57:42 +0300

    refactor(cli): update CliModule

--- app/src/main/java/com/github/alphameo/railways/application/cli/CliModule.java
+++ app/src/main/java/com/github/alphameo/railways/application/cli/CliModule.java
@@ -2,6 +2,7 @@ package com.github.alphameo.railways.application.cli;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.github.alphameo.railways.application.cli.commands.CliCommand;
 import com.github.alphameo.railways.application.cli.commands.MessageCommand;
@@ -14,8 +15,6 @@ public class CliModule {
 
     private final List<CliCommand> commands = new ArrayList<>();
     private final MessageCommand helpCmd = new MessageCommand("help", "h", "");
-    private final MessageCommand noCmd = new MessageCommand("nocmd", "n", "");
-    private String helpMsg = "";
 
     public CliModule(@NonNull final String name) {
         this.name = name;
@@ -28,22 +27,23 @@ public class CliModule {
     public void addCmd(@NonNull final CliCommand cmd) {
         commands.add(cmd);
         updateHelpMsg();
-        helpCmd.setMsg(this.helpMsg);
-        noCmd.setMsg(String.format("No such command: %s\n%s", name, this.helpMsg));
     }
 
-    public CliCommand getCmd(@NonNull final String name, @NonNull final String[] args) {
+    public Optional<CliCommand> getCommand(@NonNull final String name) {
         for (final var command : commands) {
             if (command.getName().equals(name) ||
                     command.getShortName().equals(name)) {
-                command.setArgs(args);
-                return command;
+                return Optional.of(command);
             }
         }
-        if (name.equals("help") || name.equals("h")) {
-            return helpCmd;
+        if (name.equals(helpCmd.getName()) || name.equals(helpCmd.getShortName())) {
+            return Optional.of(helpCmd);
         }
-        return noCmd;
+        return Optional.empty();
+    }
+
+    public String getHelpMessage() {
+        return this.helpCmd.getMessage();
     }
 
     private void updateHelpMsg() {
@@ -58,6 +58,6 @@ public class CliModule {
         sb.append("\t");
         sb.append(helpCmd.getSignature());
 
-        this.helpMsg = sb.toString();
+        this.helpCmd.setMessage(sb.toString());
     }
 }
